ff301f4dd6a383af0d42511c519d4bda7912b8ff
Holland Blumer <63265395+hollandblumer6@users.noreply.github.com>
2023-03-14 16:47:51 -0400

    converted to react app

---
+++ demo/src/App.js
@@ -0,0 +1,69 @@
+import React, { useRef, useState } from "react";
+import logo from './logo.svg';
+import Movie from "./assets/videos/promo.mp4";
+import './App.css';
+import Form from './components/Form'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
+
+function App() {
+
+  const videoRef = useRef();
+  const [showForm, setShowForm] = useState(false);
+
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  console.log(videoRef);
+
+  const handlePause = () => {
+    videoRef.current.pause();
+    setIsPlaying(false);
+  };
+  const handlePlay = () => {
+    videoRef.current.play();
+    setIsPlaying(true);
+
+  };
+
+  return (
+    <div className="App">
+     
+
+      <div class="left">
+      <div class="logo"> Divot </div>
+        <div class="slogan"> Sustainability, Simplified</div>
+        { showForm? (<div><Form /></div>): (<div>  
+        <div class="sign-up"     onClick={() => {
+          setShowForm(true);
+        }}>    sign up for beta here
+        </div></div>)}
+      
+      </div>
+
+      <div class="right">  
+      {" "}
+
+        {isPlaying ? (
+          <button className="pause" onClick={handlePause}>
+            <FontAwesomeIcon icon={faPause} size="10x" className="pause-btn" />
+          </button>
+        ) : (
+          <button className="btn-cover" onClick={handlePlay}>
+            <FontAwesomeIcon icon={faPlay} size="10x" className="pause-btn" />
+          </button>
+        )}
+     
+        <video id="background-video" loop ref={videoRef}>
+      <source src={Movie} type="video/mp4"></source>
+      </video> </div>
+
+
+
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ demo/src/components/Form.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import '../index.css'
+import Logo from '../assets/images/logo.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+function Form() {
+    const [suggestion, setSuggestion] = useState("");
+  return (
+<>
+<script src='https://www.google.com/recaptcha/api.js' async defer></script>
+<form action="https://formkeep.com/f/0d88812aa389"
+      accept-charset="UTF-8"
+      enctype="multipart/form-data"
+      method="POST">
+  
+    <input type="hidden" name="utf8" value="✓"  />
+    {/* <input type="name" id="name" class="search-input" name="name" placeholder="Name" /> */}
+   
+    <div className="suggestion-form">
+    <div className="suggestion-form-content">
+    <input type="email" className="suggestion-input" id="email-address" name="email" placeholder="Email" value={suggestion}   onChange={(e) => setSuggestion(e.target.value)}/>
+    <div class="g-recaptcha" data-sitekey="6Leuvf8kAAAAAKBs7ciNX-JN0I7ug6JAQryW7gPw"></div>
+   
+
+  {suggestion != "" ? (   <button type="submit" className="create-suggestion-button">   <FontAwesomeIcon
+              icon={faArrowRight}
+              size="sm"
+              color="#5bab5c"
+            />{" "}</button>):( <div className="create-suggestion-button">
+            {" "}
+            <FontAwesomeIcon
+              icon={faArrowRight}
+              size="sm"
+              color="#272626"
+            />{" "}
+          </div>)}
+          </div>
+          </div>
+    </form>
+   
+
+</>
+   
+  )
+}
+
+export default Form
